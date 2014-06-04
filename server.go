6528d6cb82d71c35bfb325cba239c699ad96e85c
Simon de Haan <simon@praekeltfoundation.org>
2014-06-04 22:17:11 +0200

    starting again

--- server.go
+++ server.go
@@ -1,13 +1,14 @@
-package unicore_network
+package main
 
 import (
 	"github.com/go-martini/martini"
 )
 
-func main() {
+func New() *martini.ClassicMartini {
 	m := martini.Classic()
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
 	m.Run()
+	return m
 }
