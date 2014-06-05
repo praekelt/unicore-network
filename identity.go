cacb1857f2fc64ab778d0ce934215e8b1035f90e
Simon de Haan <simon@praekeltfoundation.org>
2014-06-05 15:00:36 +0200

    load & save identity files

---
+++ identity.go
@@ -0,0 +1,42 @@
+package unicore_network
+
+import (
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
+	"os"
+)
+
+type Ident struct {
+	Signature   string
+	Hostname    string
+	DisplayName string
+}
+
+func CreateIdentity(signature string, hostname string, display_name string) Ident {
+	return Ident{Signature: signature, Hostname: hostname, DisplayName: display_name}
+}
+
+func SaveIdentity(identity Ident, filename string) (Ident, error) {
+	fp, err := os.Create(filename)
+	if err != nil {
+		return Ident{}, err
+	}
+	defer fp.Close()
+	return SaveIdentityFile(identity, fp)
+}
+
+func SaveIdentityFile(identity Ident, file *os.File) (Ident, error) {
+	data, err := yaml.Marshal(&identity)
+	file.Write(data)
+	return identity, err
+}
+
+func LoadIdentity(filename string) (Ident, error) {
+	ident := Ident{}
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return ident, err
+	}
+	err = yaml.Unmarshal(data, &ident)
+	return ident, err
+}
---
+++ identity_test.go
@@ -0,0 +1,45 @@
+package unicore_network
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestIdentityLoading(t *testing.T) {
+	temp_file, _ := ioutil.TempFile("", "uc")
+	defer os.Remove(temp_file.Name())
+
+	ident := Ident{Signature: "signature", Hostname: "localhost", DisplayName: "UCN"}
+	data, _ := yaml.Marshal(&ident)
+	temp_file.Write(data)
+	temp_file.Close()
+
+	i, _ := LoadIdentity(temp_file.Name())
+	if i.Signature != "signature" {
+		t.Errorf("Unexpected Signature: %s", i.Signature)
+	}
+
+	if i.Hostname != "localhost" {
+		t.Errorf("Unexpected Hostname: %s", i.Hostname)
+	}
+
+	if i.DisplayName != "UCN" {
+		t.Errorf("Unexpected DisplayName: %s", i.DisplayName)
+	}
+}
+
+func TestIdentityCreation(t *testing.T) {
+	temp_file, _ := ioutil.TempFile("", "uc")
+	defer os.Remove(temp_file.Name())
+
+	ident := CreateIdentity("signature", "hostname", "display name")
+	ident, _ = SaveIdentity(ident, temp_file.Name())
+
+	reloaded_ident, _ := LoadIdentity(temp_file.Name())
+	if ident != reloaded_ident {
+		t.Errorf("Unexpected ident: %s", reloaded_ident)
+	}
+}
