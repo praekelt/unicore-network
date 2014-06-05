7d457ab479d41c45e92d643d10a5c31705e53ac2
Simon de Haan <simon@praekeltfoundation.org>
2014-06-05 21:39:28 +0200

    test for get or create identity file

--- src/github.com/praekelt/unicore_network/server/identity_test.go
+++ src/github.com/praekelt/unicore_network/server/identity_test.go
@@ -42,3 +42,31 @@ func TestIdentityCreation(t *testing.T) {
 		t.Errorf("Unexpected ident: %s", reloaded_ident)
 	}
 }
+
+func TestGetOrCreateIdentityCreating(t *testing.T) {
+	// create a tempfile and delete it immediately but continue
+	// using the name to make sure we've got a unique file name
+	temp_file, _ := ioutil.TempFile("", "uc")
+	os.Remove(temp_file.Name())
+
+	identity, _ := GetOrCreateIdentity(temp_file.Name())
+	if identity.Signature == "" {
+		t.Error("Unexpected empty Signature")
+	}
+	if identity.Hostname == "" {
+		t.Error("Unexpected empty Hostname")
+	}
+	if identity.DisplayName == "" {
+		t.Error("Unexpected empty DisplayName")
+	}
+}
+
+func TestGetOrCreateIdentityGetting(t *testing.T) {
+	temp_file, _ := ioutil.TempFile("", "uc")
+	defer os.Remove(temp_file.Name())
+	identity, _ := SaveIdentityFile(CreateIdentity("foo", "bar", "baz"), temp_file)
+	reloaded_identity, _ := GetOrCreateIdentity(temp_file.Name())
+	if identity != reloaded_identity {
+		t.Errorf("Unexpected identity when reloading. %s vs %s", identity, reloaded_identity)
+	}
+}
