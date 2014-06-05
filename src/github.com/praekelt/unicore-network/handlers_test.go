9fa10e2b39ef09352bb5baff538736d2e1c124e8
Simon de Haan <simon@praekeltfoundation.org>
2014-06-05 09:22:35 +0200

    not using the gocheck assert helpers

--- src/github.com/praekelt/unicore-network/handlers_test.go
+++ src/github.com/praekelt/unicore-network/handlers_test.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	. "gopkg.in/check.v1"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { TestingT(t) }
-
-type MySuite struct{}
-
-var _ = Suite(&MySuite{})
-
-func (s *MySuite) TestHandlers(c *C) {
+func TestHandlers(t *testing.T) {
 	request, _ := http.NewRequest("GET", "http://localhost:3000/", nil)
 	response := httptest.NewRecorder()
 	m := martini.Classic()
 	m.Get("/", Get)
 	m.ServeHTTP(response, request)
 
-	c.Assert(response.Code, Equals, 200)
-	c.Assert(response.Body.String(), Equals, "Hello world!")
+	if response.Code != 200 {
+		t.Error("Expected 200 response code, got: %s", response.Code)
+	}
+
+	if response.Body.String() != "Hello world!" {
+		t.Error("Unexpected response: %s", response.Body.String())
+	}
 }
