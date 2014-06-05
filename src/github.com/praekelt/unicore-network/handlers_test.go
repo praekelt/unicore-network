3f29c7494106526b9716deec347f783dd687117c
Simon de Haan <simon@praekeltfoundation.org>
2014-06-05 08:58:49 +0200

    wow test passing

--- src/github.com/praekelt/unicore-network/handlers_test.go
+++ src/github.com/praekelt/unicore-network/handlers_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"github.com/go-martini/martini"
+	. "gopkg.in/check.v1"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestHandlers(t *testing.T) {
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
 
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+func (s *MySuite) TestHandlers(c *C) {
+	request, _ := http.NewRequest("GET", "http://localhost:3000/", nil)
+	response := httptest.NewRecorder()
+	m := martini.Classic()
+	m.Get("/", Get)
+	m.ServeHTTP(response, request)
+
+	c.Assert(response.Code, Equals, 200)
+	c.Assert(response.Body.String(), Equals, "Hello world!")
 }
