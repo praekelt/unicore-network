840f9ba75d2828f9059fa76bd77d3ca53d16032e
Simon de Haan <simon@praekeltfoundation.org>
2014-06-05 23:28:12 +0200

    more sideloader fiddling

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export GOPATH="${WORKSPACE}/${REPO}"
+cd "${WORKSPACE}/${REPO}"
+export GOPATH=`pwd`
 go get ./...
 go build -o ./bin/unicore_network -v github.com/praekelt/unicore_network
