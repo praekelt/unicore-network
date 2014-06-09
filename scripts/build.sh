4c7eb9009f259d99c6e540f2ad021dcd3831e28d
Simon de Haan <simon@praekeltfoundation.org>
2014-06-09 12:07:46 +0200

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
-go get ./...
+go get -t github.com/go-martini/martini
+go get -t github.com/fzzy/radix/redis
+go get -t gopkg.in/yaml.v1
 go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
