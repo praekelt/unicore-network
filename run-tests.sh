47f8c1a499385c34b5a4a716333eafe73e3039d7
Simon de Haan <simon@praekeltfoundation.org>
2014-06-09 12:46:00 +0200

    create helper for getting all project dependencies

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
+./get-dependencies.sh
 go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
 go test -v github.com/praekelt/unicore-network/...
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
-go get github.com/go-martini/martini
-go get github.com/fzzy/radix/redis
-go get gopkg.in/yaml.v1
-go get github.com/praekelt/unicore-network
+./get-dependencies.sh
 go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
