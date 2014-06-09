1bb4b100d7c27ea85008043ca8e878be4b0aa3e6
Simon de Haan <simon@praekeltfoundation.org>
2014-06-09 12:08:49 +0200

    -t not supported by sideloader

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
-go get -t github.com/go-martini/martini
-go get -t github.com/fzzy/radix/redis
-go get -t gopkg.in/yaml.v1
+go get github.com/go-martini/martini
+go get github.com/fzzy/radix/redis
+go get gopkg.in/yaml.v1
 go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
