a96c7fda86415ec0157f2996722cb8261895a70f
Simon de Haan <simon@praekeltfoundation.org>
2014-06-09 12:10:39 +0200

    manually fetch package for building

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ export GOPATH=`pwd`
 go get github.com/go-martini/martini
 go get github.com/fzzy/radix/redis
 go get gopkg.in/yaml.v1
+go get github.com/praekelt/unicore-network
 go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
