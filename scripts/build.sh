de40bf98760dd81d88ffe0a92522f033b7fe1b8c
Simon de Haan <simon@praekeltfoundation.org>
2014-06-20 16:31:41 +0200

    only copy the binary

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,5 @@
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
 ./get-dependencies.sh
-go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
-cd ..
-cp -a ${REPO} ./build/
+go build -o ${BUILD}/unicore-network -v github.com/praekelt/unicore-network
+
