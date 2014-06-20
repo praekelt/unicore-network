f2b3f7b5c52c41d1d6916c404efee9c6a427ad0a
Simon de Haan <simon@praekeltfoundation.org>
2014-06-20 16:49:47 +0200

    looks like it needs a subdir

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,6 @@
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
 ./get-dependencies.sh
-go build -o ${BUILDDIR}/unicore-network -v github.com/praekelt/unicore-network
+mkdir ${BUILDDIR}/${REPO}
+go build -o ${BUILDDIR}/${REPO}/unicore-network -v github.com/praekelt/unicore-network
 
