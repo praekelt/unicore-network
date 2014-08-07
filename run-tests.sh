74c149eaa327c42db20354227304e191f522f5de
Simon de Haan <simon@praekeltfoundation.org>
2014-08-07 21:52:47 +0200

    cleaning stuff up

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 set -e
-./get-dependencies.sh
-go build -o ./bin/unicore-network -v github.com/praekelt/unicore-network
-redis-cli -n 0 flushdb
-cd server && go test -v
-cd ..
+go test -v
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd "${WORKSPACE}/${REPO}"
 export GOPATH=`pwd`
-./get-dependencies.sh
 mkdir ${BUILDDIR}/${REPO}
 go build -o ${BUILDDIR}/${REPO}/unicore-network -v github.com/praekelt/unicore-network
-
